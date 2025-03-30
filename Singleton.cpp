2ab63cb49edaca48001ea41ac05d5ef3d81555f2
juliaamirbekyan <juliaamirbekyan@gmail.com>
2025-03-30 14:51:49 +0400

    Added Singleton design pattern

---
+++ Singleton.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Singleton {
+    static Singleton instance; // Declare the static instance
+    int num;
+
+public:
+    static Singleton& getInstance() {
+        return instance;
+    }
+
+    void showMessage() {
+        cout << "Singleton instance address: " << this << ", num: " << num << endl;
+    }
+
+private:
+    Singleton(int n = 0) : num(n) {} 
+};
+
+Singleton Singleton::instance(42);
+
+int main() {
+    Singleton& s1 = Singleton::getInstance();
+    Singleton& s2 = Singleton::getInstance();
+
+    s1.showMessage();
+    s2.showMessage();
+
+    return 0;
+}
